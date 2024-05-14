0ff5f2fcef213f96a2df349b1c561951b1899f97
Adriana FERRARI <adrifok@gmail.com>
2024-05-14 19:11:38 -0300

    update docker login

--- day-14/simple-python-app/start_container.sh
+++ day-14/simple-python-app/start_container.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 set -e
 
+# Login to Docker Hub
+docker login -u adrifok -p guate4718
+
+# Pull the Docker image
+docker pull adrifok/simple-python-flask-app
+
+# Start the container
+docker run -d -p 80:80 adrifok/simple-python-flask-app
+
 # Pull the Docker image from Docker Hub
-echo
+docker pull adrifok/simple-python-flask-app
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 adrifok/simple-python-flask-app
--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 set -e
 
+# Login to Docker Hub
+docker login -u adrifok -p guate4718
+
+# Pull the Docker image
+docker pull adrifok/simple-python-flask-app
+
+# Start the container
+docker run -d -p 80:80 adrifok/simple-python-flask-app
+
 # Pull the Docker image from Docker Hub
 docker pull adrifok/simple-python-flask-app
 
